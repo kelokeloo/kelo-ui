27d043bc14af04d6e7ec9e75b6effde6dd8be01a
kelokeloo <kelokeloo@163.com>
2022-01-25 20:05:36 +0800

    初步封装switch组件

--- src/lib/Switch/src/index.vue
+++ src/lib/Switch/src/index.vue
@@ -1,11 +1,36 @@
 <template>
   <div>
-    <!-- Switch 组件 -->
-    <!-- 1.  -->
-    <input type="checkbox">
+    <!-- Switch组件 -->
+    <!-- ✅1. 通过v-model绑定组件的真假值 -->
+    <!-- 2.  -->
+
+    switch
+    
+    <input ref="checkRef" type="checkbox" :checked="modelValue" @change="valueChange()">
+
   </div>
 </template>
 <script setup lang="ts">
+import { ref } from 'vue'
+
+interface PropsApi{
+  modelValue: boolean // 表示组件的真假值
+}
+
+const switchProps = defineProps<PropsApi>()
+
+const switchEmit = defineEmits(['update:modelValue'])
+// emitCB
+
+const valueChange = ()=>{
+  switchEmit('update:modelValue', (checkRef.value as unknown as HTMLInputElement).checked)
+}
+
+
+// checkRef
+const checkRef = ref(null)
+
+
 
 </script>
 <style scoped lang="scss">
--- src/views/doc/switch/switchDoc.vue
+++ src/views/doc/switch/switchDoc.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-    <k-switch></k-switch>
+    <k-switch v-model="bool"></k-switch>
+    {{bool}}
   </div>
 </template>
 <script setup lang="ts">
+import { ref } from 'vue'
+
+const bool = ref(true)
 
 </script>
 <style scoped lang="scss">
