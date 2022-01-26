574f65e311b67593d0ac14697aa5e6338a725c5a
kelokeloo <kelokeloo@163.com>
2022-01-26 21:41:52 +0800

    switch

--- src/lib/Switch/src/index.vue
+++ src/lib/Switch/src/index.vue
@@ -1,17 +1,19 @@
 <template>
-  <div>
+  <div class="switchBox" @click="clickBox"
+    :class="boxClassObject"
+  >
     <!-- Switch组件 -->
     <!-- ✅1. 通过v-model绑定组件的真假值 -->
-    <!-- 2.  -->
+    <!-- ✅2. 切换样式实现 -->
+    <!-- ✅3. 向外提供change回调 -->
 
-    switch
-    
-    <input ref="checkRef" type="checkbox" :checked="modelValue" @change="valueChange()">
-
+    <span :class="spanClassObject"></span>
   </div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+
+
 
 interface PropsApi{
   modelValue: boolean // 表示组件的真假值
@@ -19,20 +21,74 @@ interface PropsApi{
 
 const switchProps = defineProps<PropsApi>()
 
-const switchEmit = defineEmits(['update:modelValue'])
+const switchEmit = defineEmits(['update:modelValue', 'change'])
 // emitCB
 
-const valueChange = ()=>{
-  switchEmit('update:modelValue', (checkRef.value as unknown as HTMLInputElement).checked)
+let bool = ref(switchProps.modelValue)
+
+
+
+// active 样式
+const spanClassObject = computed(()=>{
+  return {
+    'span-active': bool.value,
+    'span-inactive': !bool.value
+  }
+})
+
+const boxClassObject = computed(()=>{
+  return {
+    'bgColor': bool.value
+  }
+})
+
+
+
+
+// inactive 样式
+
+// 点击事件的回调函数
+const clickBox = ()=>{
+  bool.value = !bool.value
+  switchEmit('update:modelValue', bool.value)
+  switchEmit('change', bool.value)
 }
-
-
-// checkRef
-const checkRef = ref(null)
-
 
 
 </script>
 <style scoped lang="scss">
+.switchBox {
+  position: relative;
+  width: 2rem;
+  height: 1.2rem;
+  border-radius: 99999px;
+  background-color: white;
+  transition: all .3s;
+  border: 1px solid #95A5A6;
+}
+.bgColor{
+  background-color: #3498DB;
+}
+
+
+.switchBox span{
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 1rem;
+  height: 1rem;
+  background-color: #BDC3C7;
+  border-radius: 50%;
+  transition: all .3s;
+}
+
+// 组件分别为真假时候的样式
+.span-active{
+  left: 40%;
+  background-color: white !important;
+}
+.span-inactive{
+  left: 10%;
+}
 
 </style>
--- src/views/doc/switch/switchDoc.vue
+++ src/views/doc/switch/switchDoc.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-    <k-switch v-model="bool"></k-switch>
+    <k-switch v-model="bool" @change="show"></k-switch>
     {{bool}}
   </div>
 </template>
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const bool = ref(true)
+const bool = ref(false)
+
+const show = (b: boolean)=>{
+  console.log('show', b);
+}
 
 </script>
 <style scoped lang="scss">
